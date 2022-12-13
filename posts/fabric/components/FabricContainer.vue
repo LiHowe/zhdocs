e5ef3d6b447d97ebddcf5e815870fc11ddc91944
LiHowe <howe.personal@gmail.com>
2022-12-13 17:33:40 +0800

    chore: update canvas width to full

--- posts/fabric/components/FabricContainer.vue
+++ posts/fabric/components/FabricContainer.vue
@@ -1,7 +1,9 @@
 <template>
-  <canvas ref="canvas" width="500" height="300"></canvas>
-  <div class="ops-container">
-    <slot name="ops"></slot>
+  <div ref="c">
+    <canvas ref="canvas" width="500" height="300"></canvas>
+    <div class="ops-container">
+      <slot name="ops"></slot>
+    </div>
   </div>
 </template>
 
@@ -17,9 +19,12 @@ const props = defineProps<{
   mounted?: (fb: typeof fabric, container: fabric.Canvas) => void
 }>()
 
+const c = ref<HTMLDivElement>()
+
 onMounted(() => {
   fb.value = new fabric.Canvas(canvas.value!, {
-    containerClass: 'fb-container'
+    containerClass: 'fb-container',
+    width: c.value?.clientWidth
   })
   props.mounted && props.mounted(fabric, fb.value)
 })
