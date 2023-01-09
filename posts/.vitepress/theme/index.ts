96888d6f854a678624858cb955028153f5de3030
LiHowe <howe.personal@gmail.com>
2023-01-09 16:15:00 +0800

    chore: add element-plus as components lib

--- posts/.vitepress/theme/index.ts
+++ posts/.vitepress/theme/index.ts
@@ -7,6 +7,9 @@ import Button from '../components/Btn.vue'
 import Checkbox from '../components/Cbx.vue'
 import Icon from '../components/Icon.vue'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import './main.scss'
 // import './iconfont.css'
 
@@ -20,5 +23,6 @@ export default {
     ctx.app.component('Btn', Button)
     ctx.app.component('Cbx', Checkbox)
     ctx.app.component('Icon', Icon)
+    ctx.app.use(ElementPlus, { size: 'small' })
   }
 }
